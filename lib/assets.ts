3c9caa2b813e1bd2042d07efa949d0a80658ed09
Julia Jakob <juliajakob2106@gmail.com>
2026-08-14 13:52:58 -0400

    Converting asset formats

---
+++ lib/assets.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+function toBase64(filePath: string) {
+    const file = fs.readFileSync(filePath);
+    return `data:image/png;base64,${file.toString("base64")}`;
+}
+
+export const ASSETS = {
+    sleepingFrog: toBase64(path.join(process.cwd(), "public/assets/sleeping_frog.png")),
+    awakeFrog: toBase64(path.join(process.cwd(), "public/assets/awake_frog.png")),
+    box: toBase64(path.join(process.cwd(), "public/assets/box.png")),
+};
